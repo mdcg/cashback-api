3939016b5e37c9b2d076712cd1f0c910f4d8bf79
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-09-04 00:02:50 -0300

    feat: creating Dockerfile and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10-alpine
+
+MAINTAINER MDCG
+ENV PYTHONUNBUFFERED 1
+
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
+
+COPY ./requirements.txt /code/requirements.txt
+RUN pip install -r /code/requirements.txt
+
+COPY . /opt/code/
+WORKDIR /opt/code/
---
+++ scripts/create_postgresql_db.py
@@ -0,0 +1,59 @@
+import logging
+from contextlib import contextmanager
+from os import getenv
+
+import psycopg2
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        fmt="[database_script] %(asctime)s - %(levelname)s : %(message)s",
+        datefmt="%d/%m/%Y %I:%M:%S %p",
+    )
+)
+logger.addHandler(handler)
+
+POSTGRESQL_URI = getenv("POSTGRESQL_URI")
+
+
+@contextmanager
+def get_connection():
+    conn = psycopg2.connect(POSTGRESQL_URI)
+    cursor = conn.cursor()
+    try:
+        yield conn, cursor
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def create_cashback_db():
+    logger.info("Creating Cashback database...")
+
+    with get_connection() as conn, cur:
+        conn = psycopg2.connect(POSTGRESQL_URI)
+        cur = conn.cursor()
+        try:
+            cur.execute(
+                (
+                    "CREATE TABLE resellers "
+                    "(id serial PRIMARY KEY, fullname varchar, "
+                    "cpf varchar, email varchar, password varchar);"
+                )
+            )
+            cur.execute(
+                (
+                    "CREATE TABLE sales "
+                    "(id serial PRIMARY KEY, code varchar, date TIMESTAMP, "
+                    "value decimal, reseller_cpf varchar);"
+                )
+            )
+            conn.commit()
+        except psycopg2.DatabaseError as error:
+            logger.error(error)
+
+
+if __name__ == "__main__":
+    create_cashback_db()
