b7ef6b7678f97296528ac2e9c1a90df678c55de6
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-09-05 02:05:33 -0300

    feat: creating consumer logic

--- mock_workers/consumer.py
+++ mock_workers/consumer.py
@@ -4,13 +4,16 @@ nosso worker. Para deixar um pouco dinâmico, uma compra terá
 uma chance em dez de ser negada.
 """
 
+import json
 import logging
 import random
+import time
 from contextlib import contextmanager
 from os import getenv
 
 import pika
 import psycopg2
+import psycopg2.extras
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -27,10 +30,16 @@ RABBITMQ_HOST = getenv("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = int(getenv("RABBITMQ_PORT", 5672))
 RABBITMQ_USER = getenv("RABBITMQ_USER", "guest")
 RABBITMQ_PASS = getenv("RABBITMQ_PASS", "guest")
+SALES_QUEUE_NAME = getenv("SALES_QUEUE_NAME", "sales_queue")
 
 POSTGRESQL_URI = getenv(
     "POSTGRESQL_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
+
+
+def random_status():
+    number = random.randint(0, 10)
+    return "Negado" if number == 3 else "Aprovado"
 
 
 @contextmanager
@@ -45,7 +54,10 @@ def get_connection():
 
 
 def process_sale(reseller_cpf, sale_code):
-    with get_connection() as (_, cur):
+    with get_connection() as (conn, cur):
+        # Aguarda um tempinho, para simular um processamento.
+        time.sleep(5)
+
         cur.execute(
             "SELECT * FROM sales WHERE reseller_cpf=%s AND code=%s;",
             (reseller_cpf, sale_code),
@@ -55,13 +67,23 @@ def process_sale(reseller_cpf, sale_code):
             logger.info(f"Sale {sale_code} not found.")
             return None
 
-        # cur.execute(
-        #     "UPDATE sales SET status=%s WHERE reseller_cpf=%s AND code=%s;", (status, reseller_cpf, sale_code),
-        # )
+        status = random_status()
+
+        logger.info(
+            f"Venda {sale_code} - Revendedor {reseller_cpf} - Status {status}"
+        )
+
+        cur.execute(
+            "UPDATE sales SET status=%s WHERE reseller_cpf=%s AND code=%s;",
+            (random_status(), reseller_cpf, sale_code),
+        )
+        conn.commit()
 
 
 def callback(ch, method, properties, body):
     logger.info("Message received...")
+    body = json.loads(body)
+    process_sale(reseller_cpf=body["reseller_cpf"], sale_code=body["code"])
     return ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
@@ -73,9 +95,10 @@ def run():
     )
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.queue_declare(queue="sales_queue")
+    channel.queue_declare(queue=SALES_QUEUE_NAME)
     channel.basic_consume(
-        queue="sales_queue", on_message_callback=callback, auto_ack=True
+        queue=SALES_QUEUE_NAME,
+        on_message_callback=callback,
     )
     channel.start_consuming()
 
